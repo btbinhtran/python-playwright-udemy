2499ec484e19d6cd9755b0133c37c5356cb51c59
Binh Tran <btbinhtran@gmail.com>
2024-03-24 18:06:53 -0700

    Add test_verify_text.

---
+++ ui_testing_playground/test_verify_text.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import Page, expect
+
+def test_contains_text(page: Page):
+  page.goto("http://uitestingplayground.com/verifytext")
+
+  text = page.locator("div.bg-primary").get_by_text("Welcome")
+
+  expect(text).to_have_text("Welcome UserName!")

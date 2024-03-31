7d66d0eb2f0406b08ba7192a5eaf0013e426410f
Binh Tran <btbinhtran@gmail.com>
2024-03-30 19:42:15 -0700

    Add first working async test with fixture.

--- async_ui_testing_playground/test_async_class_attribute.py
+++ async_ui_testing_playground/test_async_class_attribute.py
@@ -1,15 +1,30 @@
 import pytest
 from playwright.async_api import async_playwright, expect
 
+
+@pytest.fixture(autouse=True)
+async def browser():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)  # Change browser if needed
+        yield browser
+        await browser.close()
+
+
+@pytest.fixture(autouse=True)
+async def page(browser):
+    async for b in browser:
+      page = await b.new_page()
+      yield page
+      await page.close()
+
+
 @pytest.mark.asyncio
-async def test_async_class_attribute():
-  async with async_playwright() as p:
-    browser = await p.chromium.launch(headless=False)
-    page = await browser.new_page()
-    await page.goto("http://uitestingplayground.com/classattr")
+async def test_async_class_attribute(page):
+    async for p in page:
+      await p.goto("http://uitestingplayground.com/classattr")
+      
+      primary_btn = p.locator("button.btn-primary")
 
-    primary_btn = page.locator("button.btn-primary")
+      await expect(primary_btn).to_be_visible()
 
-    await expect(primary_btn).to_be_visible()
-
-    await primary_btn.click()
+      await primary_btn.click()

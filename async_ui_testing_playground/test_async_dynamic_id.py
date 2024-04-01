fbe6956ddede915030345c8a6879bdf6b66e45d7
Binh Tran <btbinhtran@gmail.com>
2024-04-01 06:49:34 -0700

    Fix test_async_dynamic_id.py

--- async_ui_testing_playground/test_async_dynamic_id.py
+++ async_ui_testing_playground/test_async_dynamic_id.py
@@ -1,15 +1,32 @@
-import pytest
+import pytest, pytest_asyncio
 from playwright.async_api import async_playwright, expect
 
-@pytest.mark.asyncio
-async def test_dynamic_id():
+@pytest_asyncio.fixture(autouse=True)
+async def browser():
   async with async_playwright() as p:
     browser = await p.chromium.launch(headless=False)
-    page = await browser.new_page()
-    await page.goto("http://uitestingplayground.com/dynamicid")
+    yield browser
+    await browser.close()
 
-    button = page.get_by_role(
-      "button", name="Button with Dynamic ID"
-    )
-    await expect(button).to_be_visible()
-    await button.click()
+@pytest_asyncio.fixture(autouse=True)
+async def context(browser):
+  context = await browser.new_context()
+  yield context
+  await context.close()
+
+@pytest_asyncio.fixture(autouse=True)
+async def page(context):
+  page = await context.new_page()
+  yield page
+  await page.close()
+
+
+@pytest.mark.asyncio
+async def test_dynamic_id(page):
+  await page.goto("http://uitestingplayground.com/dynamicid")
+
+  button = page.get_by_role(
+    "button", name="Button with Dynamic ID"
+  )
+  await expect(button).to_be_visible()
+  await button.click()

017dcdb6d51c5fc1a196d07756b4781dbc160072
Binh Tran <btbinhtran@gmail.com>
2024-04-01 06:53:53 -0700

    Refactor fixtures into conftest.py

--- async_ui_testing_playground/test_async_class_attribute.py
+++ async_ui_testing_playground/test_async_class_attribute.py
@@ -1,24 +1,5 @@
-import pytest, pytest_asyncio
-from playwright.async_api import async_playwright, expect
-
-@pytest_asyncio.fixture(autouse=True)
-async def browser():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
-        yield browser
-        await browser.close()
-
-@pytest_asyncio.fixture(autouse=True)
-async def context(browser):
-    context = await browser.new_context()
-    yield context
-    await context.close()
-
-@pytest_asyncio.fixture(autouse=True)
-async def page(context):
-    page = await context.new_page()
-    yield page
-    await page.close()
+import pytest
+from playwright.async_api import expect
 
 @pytest.mark.asyncio
 async def test_async_class_attribute(page):

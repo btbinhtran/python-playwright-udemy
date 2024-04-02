2aa0096d7c10464f9308be2dff3f13cad702d86c
Binh Tran <btbinhtran@gmail.com>
2024-04-02 07:42:03 -0700

    Add async verify text test.

---
+++ async_ui_testing_playground/test_async_verify_text.py
@@ -0,0 +1,10 @@
+import pytest, re
+from playwright.async_api import Page, expect
+
+@pytest.mark.asyncio
+async def test_async_verify_text(page: Page):
+  await page.goto("http://uitestingplayground.com/verifytext")
+
+  welcome_text = page.locator("div.bg-primary").get_by_text("Welcome")
+
+  await expect(welcome_text).to_have_text("Welcome UserName!")

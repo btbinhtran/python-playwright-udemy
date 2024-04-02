dc7d98b05902f165a3bc3d1da53ca9643dbc1856
Binh Tran <btbinhtran@gmail.com>
2024-04-02 10:35:41 -0700

    Add async nbsp non-breaking space character test.

---
+++ async_ui_testing_playground/test_async_nbsp.py
@@ -0,0 +1,11 @@
+import pytest
+from playwright.async_api import Page, expect
+
+@pytest.mark.asyncio
+async def test_async_nbsp(page: Page):
+  await page.goto("http://uitestingplayground.com/nbsp")
+
+  # Look on https://en.wikipedia.org/wiki/List_of_Unicode_characters
+  # to figure out which unicode character matches to the HTML character
+  btn = page.locator("//button[text()='My\u00a0Button']")
+  await expect(btn).to_be_visible()

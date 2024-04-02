c61c23b97bd4abb813c07eea7c8d3c086a70e0ea
Binh Tran <btbinhtran@gmail.com>
2024-04-02 11:58:32 -0700

    Add test_async_solver_with_image_challenge.

--- async_ui_testing_playground/test_async_recaptchav2.py
+++ async_ui_testing_playground/test_async_recaptchav2.py
@@ -1,8 +1,8 @@
-# This is an example of using a RECAPTCHAv2 Solving Service.
-# This is not the best way to test a web form that has RECAPTCHA.
-# * You would want to have a test mode that doesn't show the RECAPTCHA.
+# This is an example of using a reCAPTCHAv2 Solving Service.
+# This is not the best way to test a web form that has reCAPTCHA.
+# * You would want to have a test mode that doesn't show the reCAPTCHA.
 # * You can try to mock the response from the server to bypass the actual
-#   RECAPTCHA solving step.
+#   reCAPTCHA solving step.
 
 import pytest
 from playwright.async_api import Page, expect
@@ -15,8 +15,29 @@ from playwright_recaptcha import (
 
 @pytest.mark.asyncio
 @pytest.mark.xfail(raises=RecaptchaRateLimitError)
-async def test_async_recaptchav2(page: Page):
+async def test_async_solver_with_normal_recaptcha(page: Page):
   """Test the solver with a normal reCAPTCHA."""
   await page.goto("https://www.google.com/recaptcha/api2/demo")
   async with recaptchav2.AsyncSolver(page) as solver:
     await solver.solve_recaptcha(wait=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.xfail(raises=(RecaptchaNotFoundError, RecaptchaRateLimitError))
+async def test_async_solver_with_hidden_recaptcha(page: Page):
+  """Test the solver with a hidden reCAPTCHA"""
+  await page.goto("https://www.google.com/recaptcha/api2/demo?invisible=true")
+  submit_btn = page.get_by_role("button", name="Submit")
+  await submit_btn.click()
+
+  async with recaptchav2.AsyncSolver(page) as solver:
+    await solver.solve_recaptcha(wait=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.xfail(raises=CapSolverError)
+async def test_async_solver_with_image_challenge(page: Page):
+  await page.goto("https://www.google.com/recaptcha/api2/demo")
+
+  async with recaptchav2.AsyncSolver(page) as solver:
+    await solver.solve_recaptcha(wait=True, image_challenge=True)

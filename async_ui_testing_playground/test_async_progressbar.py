17770d43cc0f70daafefe819ce23408b22f3f117
Binh Tran <btbinhtran@gmail.com>
2024-04-02 07:49:43 -0700

    Add async progress bar test.

---
+++ async_ui_testing_playground/test_async_progressbar.py
@@ -0,0 +1,27 @@
+import pytest
+from playwright.async_api import Page, expect
+
+@pytest.mark.asyncio
+async def test_async_progressbar(page: Page):
+  await page.goto("http://uitestingplayground.com/progressbar")
+  stop_percentage = 75
+  value_now = None
+
+  progressbar = page.get_by_role("progressbar")
+
+  start_btn = page.get_by_role("button", name="Start")
+  stop_btn = page.get_by_role("button", name="Stop")
+
+  await start_btn.click()
+
+  while True:
+    valuenow = int(await progressbar.get_attribute("aria-valuenow"))
+
+    if valuenow >= stop_percentage:
+      break
+    else:
+      print(f"Percent: {valuenow}%")
+
+  await stop_btn.click()
+
+  assert valuenow >= stop_percentage

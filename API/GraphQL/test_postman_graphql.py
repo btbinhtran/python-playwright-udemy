ae645271732b192eb8d2e5ac884ad0accc64c55b
Binh Tran <btbinhtran@gmail.com>
2024-04-03 16:19:08 -0700

    Add bad query test.

--- API/GraphQL/test_postman_graphql.py
+++ API/GraphQL/test_postman_graphql.py
@@ -1,4 +1,4 @@
-from playwright.async_api import async_playwright, APIRequestContext, Playwright
+from playwright.async_api import async_playwright, APIRequestContext, expect, Playwright
 import pytest, pytest_asyncio, json
 
 
@@ -36,3 +36,36 @@ async def test_graphql_echo(api_context: APIRequestContext):
   print("DATADATADATA")
   print(data)
   assert data["data"]["hello"] == "Hello Binh"
+
+
+@pytest.mark.asyncio
+async def test_graphql_bad_query(api_context: APIRequestContext):
+  # Simple query to test fetching a hello message
+  query = """
+  query Hello {
+    hello
+  """
+
+  # Send the GraphQL request using api_context
+  response = await api_context.post(
+    "", # I put the graphql endpoint in the base_url but you still need to pass a blank url string
+    data=json.dumps({"query": query})
+  )
+
+  status_code = response.status
+  assert status_code == 200, f"Unexpected status code: {status_code}"
+
+  data = await response.json()
+
+  assert "errors" in data
+  assert isinstance(data["errors"], list)
+  assert len(data["errors"]) == 1
+  assert "message" in data["errors"][0]
+  assert "locations" in data["errors"][0]
+  assert data["errors"][0]["message"] == 'Syntax Error: Expected Name, found <EOF>.'
+  assert isinstance(data["errors"][0]["locations"], list)
+  assert len(data["errors"][0]["locations"]) == 1
+  assert "line" in data["errors"][0]["locations"][0]
+  assert "column" in data["errors"][0]["locations"][0]
+  assert data["errors"][0]["locations"][0]["line"] == 4
+  assert data["errors"][0]["locations"][0]["column"] == 3

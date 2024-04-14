c82a66a3f8cdd766ce9c8163da6bd10dbdf3ec01
Binh Tran <btbinhtran@gmail.com>
2024-04-14 11:41:12 -0700

    Fix flaky test_overlapped_element.py

--- ui_testing_playground/test_overlapped_element.py
+++ ui_testing_playground/test_overlapped_element.py
@@ -8,9 +8,10 @@ def test_overlapped(page: Page):
   div = input.locator("..")
   div.hover()
 
-  page.mouse.wheel(0, 200)
+  page.mouse.wheel(0, 200) # Page scrolling can cause flakiness and you have to do a manual wait afterwards
 
   data = "python"
+  page.wait_for_selector("input[placeholder='Name']") # Fix flakiness that makes sure the input is editable after mouse scrolls
   input.fill(data)
 
   div.screenshot(path="test-overlapped.jpg")

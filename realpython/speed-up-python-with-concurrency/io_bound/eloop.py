8c72485a407a32e5894a20467d386e63277c2fcb
Binh Tran <btbinhtran@gmail.com>
2024-05-12 19:26:36 -0700

    Add batch upload task example

--- realpython/speed-up-python-with-concurrency/io_bound/eloop.py
+++ realpython/speed-up-python-with-concurrency/io_bound/eloop.py
@@ -15,6 +15,13 @@ async def download_all_sites(sites):
             tasks.append(task)
 
         await asyncio.gather(*tasks, return_exceptions=True)
+
+# Example to batch upload tasks 10 at a time
+async def upload_files(tasks):
+    batch_size = 10
+    for i in range(0, len(tasks), batch_size):
+        batch_tasks = tasks[i:i+batch_size]
+        await asyncio.gather(*batch_tasks)
     
 
 if __name__ == '__main__':
